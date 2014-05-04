b9d53fb8b2cd895eacc96167a72fcc899219caa3
GhostTW <ghosttw88@hotmail.com>
2014-05-04 19:55:19 +0800

    自動判斷斷行 commit 錯誤

--- assets/js/function.js
+++ assets/js/function.js
@@ -18,80 +18,6 @@ autoheight($("#ta"));
 
 function myFunction()
 {
-var userText = document.getElementById("userText").value;
-var lineLength = document.getElementById("lineLength").value;
-
-if(userText.length%lineLength!=0)
-{
-	var spaceNum = lineLength-((userText.length)%lineLength);
-
-	for (var i=0;i<spaceNum;i++)
-	{
-		userText+=String.fromCharCode(32);
-	}
-}
-
-var temp = 0;
-var textTemp = "";
-
-for(var i=0;i<userText.length;i++)
-{
-	console.info(userText.charCodeAt(i)+":"+userText[i]);
-	if(userText.charCodeAt(i)==32)
-	{
-		temp=12288;
-	}
-	else if(userText.charCodeAt(i)>=33&&userText.charCodeAt(i)<=126)
-	{
-		temp=userText.charCodeAt(i)+65248;
-	}
-	else
-	{
-		temp=userText.charCodeAt(i);
-	}
-	textTemp+=String.fromCharCode(temp);
-
-}
-
-var lines = textTemp.length/lineLength;
-var TP1 = new Array(lines);
-for(var i=0;i<lines;i++)
-{
-	TP1[i] = new Array(lineLength);
-}
-var TP2 = new Array(lines);
-for(var i=0;i<lines;i++)
-{
-	TP2[i] = new Array(lineLength);
-}
-
-var TP1count=0;
-for(var i=0;i<lines;i++)
-{
-	for(var j=0;j<lineLength;j++)
-	{
-		TP1[i][j]=textTemp.charAt(TP1count);
-		TP1count++;
-	}
-}
-
-var x = "";
-
-for(var i=0;i<lineLength;i++)
-{
-	for(var j=lines-1;j>=0;j--)
-	{
-		x = x.concat(TP1[j][i]);
-		x += String.fromCharCode(32);
-	}
-	x = x.concat('\n');
-}
-
-document.getElementById("output").innerHTML = x;
-}
-
-function myFunction1()
-{
 	var FullSpace = '　';
 	var userText = document.getElementById("userText").value;
 
@@ -101,27 +27,20 @@ function myFunction1()
 
 	var maxLength = Math.max.apply(Math, $.map(messages, function (el) { return el.length }));
 
-	var result = Array.apply(null,new Array(maxLength)).map(String.prototype.valueOf,"") //new Array(maxLength);
-
-	//console.log("maxLength: "+maxLength);
-	//console.log("result: "+result);
+	var result = Array.apply(null,new Array(maxLength)).map(String.prototype.valueOf,"")
 
 	var row = 0;
 
 	for (var inputRow = messages.length - 1; inputRow >= -1; inputRow--) 
 	{
-		//console.log("inputRow: "+inputRow);
 		for (var inputColumn = 0; inputColumn < maxLength; inputColumn++) 
 		{
-			//console.log("inputColumn: "+inputColumn);
-			//console.log("messages[inputRow].Length: "+messages[inputRow].Length);
 			if(inputRow == -1)
 			{
 				result[row++] += "\n"
 			}
 			else if (inputColumn < messages[inputRow ].length) 
 			{
-				//console.log("messages["+inputRow+"]["+inputColumn+"]: "+messages[inputRow][inputColumn]);
 				var code = messages[inputRow].charCodeAt(inputColumn);
 				if(code>=33&&code<=126)
 				{
