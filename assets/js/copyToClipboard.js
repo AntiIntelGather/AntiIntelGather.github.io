c47991a5d9e14852a2f9aa8de1cff00fc4a05a16
GhostTW <ghosttw88@hotmail.com>
2014-05-05 02:09:38 +0800

    test

--- assets/js/copyToClipboard.js
+++ assets/js/copyToClipboard.js
@@ -1,9 +1,23 @@
 (function() {
-        var clip = new ZeroClipboard($("CopyButton"));
-        clip.on("copy",function() {
-            clip.setText(ZeroClipboard.$('#output').val());
-            debugstr("copied!");
-        });
+        var client = new ZeroClipboard(document.getElementById('CopyButton') );
+
+        
+      client.on( 'ready', function(event) {
+        // console.log( 'movie is loaded' );
+
+        client.on( 'copy', function(event) {
+          event.clipboardData.setData('text/plain', event.target.innerHTML);
+        } );
+
+        client.on( 'aftercopy', function(event) {
+          console.log('Copied text to clipboard: ' + event.data['text/plain']);
+        } );
+      } );
+
+      client.on( 'error', function(event) {
+        console.log( 'ZeroClipboard error of type "' + event.name + '": ' + event.message );
+        ZeroClipboard.destroy();
+      } );
 })();
 
 	// jquery stuff(optional)
