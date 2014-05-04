d058c61699c3fe0382726300e5062dcfb0f2cf87
GhostTW <ghosttw88@hotmail.com>
2014-05-05 01:48:46 +0800

    test copy to clipboard

--- assets/js/copyToClipboard.js
+++ assets/js/copyToClipboard.js
@@ -1,41 +1,14 @@
-(function(){
-
-	/* 全局设置方法
-	ZeroClipboard.setDefaults({
-		moviePath:"javascripts/ZeroClipboard.swf"
-	});
-	
-	var clip=new ZeroClipboard(document.getElementById("CopyButton"));
-	或者
-	var clip=new ZeroClipboard();
-	clip.glue(document.getElementById("CopyButton"));
-	*/
-
-	var clip=new ZeroClipboard($("CopyButton"),{
-		moviePath:"./assets/js/ZeroClipboard.swf"
-	});
-	
-	clip.on('ready',function(client){
-		debugstr("Flash 文件已经载入");
-	});
-	
-	clip.on('noFlash',function(client,args){
-		$(".demo-area").hide();
-		debugstr("您的浏览器没有使用 Flash");
-	});
-	
-	clip.on('wrongFlash',function(client,args){
-		$(".demo-area").hide();
-		debugstr("Flash 版本需要 10.0.0+，当前 Flash 版本为 " + args.flashVersion.replace(/,/g,"."));
-	});
-
-	clip.on('complete',function(client,args){
-		debugstr("已复制到剪贴板内容为：" + args.text);
-	});
+(function() {
+        var clip = new ZeroClipboard.Client();
+        clip.setHandCursor(true);
+        clip.glue('CopyButton');
+        clip.addEventListener('mouseDown',function() {
+            clip.setText(ZeroClipboard.$('#output').val());
+            debugstr("copied!");
+        });
+})()
 
 	// jquery stuff(optional)
 	function debugstr(text){
 		$("#d_debug").append($("<p>").text(text));
 	};
-
-})();
