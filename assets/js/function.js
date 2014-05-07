d039faef5c4504b69460df0ac677bb3135025ec3
Kuan Hsueh Lai <mlkh0225@gmail.com>
2014-05-08 04:50:54 +0800

    function.js 將轉換 function 拆解，方便新增不同轉換模式 新增設定直行數轉換 index.html 增加不同轉換模式選項

--- assets/js/function.js
+++ assets/js/function.js
@@ -16,14 +16,76 @@ function autoheight(a) {
 
 autoheight($("#ta"));
 
+function getOption()
+{
+	var byChars = document.getElementById("byChars");
+	if(byChars.checked==true)
+	{
+		return byChars.id;
+	}
+
+	var byLines = document.getElementById("byLines");
+	if(byLines.checked==true)
+	{
+		return byLines.id;
+	}
+}
+
+//Convert half-width(HW) characters into full-width(FW)
+function halfToFull(text)
+{
+	var temp = 0;
+	//This variable will hold the converted text
+	var tempText = "";
+
+	for(var i=0;i<text.length;i++)
+	{
+		//Replace all HW space with FW
+		if(text.charCodeAt(i)==32)
+		{
+			temp=12288;
+		}
+		else if(text.charCodeAt(i)>=33&&text.charCodeAt(i)<=126)
+		{
+			temp=text.charCodeAt(i)+65248;
+		}
+		else
+		{
+			temp=text.charCodeAt(i);
+		}
+		tempText+=String.fromCharCode(temp);
+	}
+	
+	return tempText;
+}
+
 function myFunction()
 {
-var userText = document.getElementById("userText").value;
-var lineLength = document.getElementById("lineLength").value;
+	var choice = getOption();
 
-if(userText.length%lineLength!=0)
+	if(choice=="byChars")
+	{
+		byChars();
+	}
+	if(choice=="byLines")
+	{
+		byLines();
+	}
+}
+
+function byLines()
 {
-	var spaceNum = lineLength-((userText.length)%lineLength);
+var userText = document.getElementById("userText").value;
+var lines = document.getElementById("lines").value;
+
+if(userText.length%lines!=0)
+{
+	var totolLength = userText.length;
+	while(totolLength%lines!=0)
+	{
+		totolLength++;
+	}
+	var spaceNum = totolLength-userText.length;
 
 	for (var i=0;i<spaceNum;i++)
 	{
@@ -31,27 +93,10 @@ if(userText.length%lineLength!=0)
 	}
 }
 
-var temp = 0;
-var textTemp = "";
+var lineLength = userText.length/lines;
 
-for(var i=0;i<userText.length;i++)
-{
-	if(userText.charCodeAt(i)==32)
-	{
-		temp=12288;
-	}
-	else if(userText.charCodeAt(i)>=33&&userText.charCodeAt(i)<=126)
-	{
-		temp=userText.charCodeAt(i)+65248;
-	}
-	else
-	{
-		temp=userText.charCodeAt(i);
-	}
-	textTemp+=String.fromCharCode(temp);
-}
+userText = halfToFull(userText);
 
-var lines = textTemp.length/lineLength;
 var TP1 = new Array(lines);
 for(var i=0;i<lines;i++)
 {
@@ -68,7 +113,62 @@ for(var i=0;i<lines;i++)
 {
 	for(var j=0;j<lineLength;j++)
 	{
-		TP1[i][j]=textTemp.charAt(TP1count);
+		TP1[i][j]=userText.charAt(TP1count);
+		TP1count++;
+	}
+}
+
+var x = "";
+
+for(var i=0;i<lineLength;i++)
+{
+	for(var j=lines-1;j>=0;j--)
+	{
+		x = x.concat(TP1[j][i]);
+		x += String.fromCharCode(32);
+	}
+	x = x.concat('\n');
+}
+
+document.getElementById("output").innerHTML = x;
+}
+
+function byChars()
+{
+var userText = document.getElementById("userText").value;
+var lineLength = document.getElementById("lineLength").value;
+
+if(userText.length%lineLength!=0)
+{
+	var spaceNum = lineLength-((userText.length)%lineLength);
+
+	for (var i=0;i<spaceNum;i++)
+	{
+		userText+=String.fromCharCode(32);
+	}	
+}
+
+var lines = userText.length/lineLength;
+
+userText = halfToFull(userText);
+
+var TP1 = new Array(lines);
+for(var i=0;i<lines;i++)
+{
+	TP1[i] = new Array(lineLength);
+}
+var TP2 = new Array(lines);
+for(var i=0;i<lines;i++)
+{
+	TP2[i] = new Array(lineLength);
+}
+
+var TP1count=0;
+for(var i=0;i<lines;i++)
+{
+	for(var j=0;j<lineLength;j++)
+	{
+		TP1[i][j]=userText.charAt(TP1count);
 		TP1count++;
 	}
 }
